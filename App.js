699a92dd35358e1bc58d01fe4192359b63a632c4
Juan Romero <75763058+jromerooo2@users.noreply.github.com>
2023-01-01 20:56:26 -0600

    accessing the webcam

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
-import { useState } from 'react';
-import { StyleSheet, View, FlatList, Button, Text } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, View, Button, Text , TouchableOpacity } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import GoalItem from './components/GoalItem';
 import GoalInput from './components/GoalInput';
 import SpecificSubject from './views/SpecificSubject';
+import { Camera, CameraType } from 'expo-camera';
 
 export default function App() {
   const [modalIsVisible, setModalIsVisible] = useState(false);
   const [idPeriod, setIdPeriod] = useState(1);
-
+  const [type, setType] = useState(CameraType.back);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+  function toggleCameraType() {
+    setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+  }
   const subjects_complete = [
 {
       "period": 1,
@@ -54,6 +59,8 @@ export default function App() {
       "time": "1:30-2:24"
   }
   ]
+
+
   // console.log(subjects_complete.Math.teacher);
   function startAddGoalHandler(id) {
     // console.log(id);
@@ -81,56 +88,45 @@ export default function App() {
 
   return (
     <>
-      <StatusBar style="light" />
       <View style={styles.appContainer}>
-        <Text style={styles.title}>My Schedule</Text>
-        <GoalInput
-          visible={modalIsVisible}
-          onCancel={endAddGoalHandler}
-          idPeriod={idPeriod}
-        />
-        <View style={styles.goalsContainer}>
-          <FlatList
-            data={subjects_complete}
-            renderItem={(itemData) => {
-              return (
-                <GoalItem
-                  teacher={itemData.item.teacher}                  
-                  time={itemData.item.time}
-                  period={itemData.item.period}
-                  pressed={startAddGoalHandler}
-                  onClickItem={startAddGoalHandler}                  
-                />
-              );
-            }}
-            keyExtractor={(item, index) => {
-              return item.period;
-            }}
-            alwaysBounceVertical={false}
-          />
+        <Text style={styles.title}>Webcam API</Text>
+        <View style={styles.container}>
+          <Camera style={styles.camera} type={type}>
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
+                <Text style={styles.text}>Flip Camera</Text>
+              </TouchableOpacity>
+            </View>
+          </Camera>
         </View>
-        {/* <View style={styles.goalsContainer}>
-          <FlatList
-            data={subjects_complete}
-            renderItem={(obj) => {
-              return (
-                <SubjectItem
-                  teacher={obj.teacher}
-                />
-              );
-            }}
-            keyExtractor={(item, index) => {
-              return item.id;
-            }}
-            alwaysBounceVertical={false}
-          />
-        </View> */}
       </View>
     </>
   );
 }
 
 const styles = StyleSheet.create({
+  button: {
+    flex: 0.1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+    height: 100,
+    backgroundColor: 'blue',
+  },
+  camera: {
+    flex: 1,
+    width: '100%',
+  },
+  buttonContainer: {
+    width: '100%',
+    flex: 100,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   appContainer: {
     flex: 1,
     paddingTop: 50,
@@ -142,8 +138,5 @@ const styles = StyleSheet.create({
     marginBottom: 16,
     color: 'white',
     textAlign: 'center',
-  },
-  goalsContainer: {
-    flex: 5,
   },
 });
